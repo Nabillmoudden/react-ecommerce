e048d9a05ae07f72f30611018976f0b8836eeb80
Nabillmoudden <importantemail012@gmail.com>
2023-12-16 22:30:50 +0100

    react ecommerce

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Navbar } from "../components";
+const AboutPage = () => {
+  return (
+    <>
+    <Navbar />
+      <div className="container my-3 py-3">
+        <h1 className="text-center">About Us</h1>
+        <hr />
+        <p className="lead text-center">
+          
+Welcome to TechTrend Hub - Your Gateway to Cutting-Edge Electronics!
+At TechTrend Hub, we're passionate about bringing you the latest in technology, innovation, and convenience. From state-of-the-art gadgets to must-have accessories, we're your one-stop destination for all things electronics.
+        </p>
+      </div>
+    </>
+  )
+}
+export default AboutPage
